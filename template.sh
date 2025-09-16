9b35808aa34eb0ef539f265efc630fa3d00133fe
Jihan <jihan.ghanim911@gmail.com>
2025-09-16 20:23:19 +0300

    folder structure edit

---
+++ template.sh
@@ -0,0 +1,17 @@
+#create directory
+mkdir -p scr
+mkdir -p research
+
+#create files
+touch scr/__init__.py
+touch scr/helper.py
+touch scr/prompt.py
+touch .env
+touch requirements.txt
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+
+echo "Project structure created successfully." 
+
+#use sh template.sh to create the structure on git bash terminal
