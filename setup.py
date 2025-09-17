6d507d8b4d5ef3d05c09d5d91157c9387da429ee
Jihan <jihan.ghanim911@gmail.com>
2025-09-17 19:48:15 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author='Jihan Ghanim',
+    email='jihan.ghanim911@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
