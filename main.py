1377e596e0a336456add2e7973d4e6c84c2184d4
Jihan <jihan.ghanim911@gmail.com>
2026-01-15 16:14:50 +0200

    data preprocessing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from app.services.data_loader import download_data, load_pdf
+from app.services.data_loader import load_pdf
+from app.services.data_processor import medical_filter_docs
 
 if __name__ == "__main__":
     
@@ -6,5 +7,8 @@ if __name__ == "__main__":
     url = "https://staibabussalamsula.ac.id/wp-content/uploads/2024/06/The-Gale-Encyclopedia-of-Medicine-3rd-Edition-staibabussalamsula.ac_.id_.pdf"
     pdfname = "The-Gale-Encyclopedia-of-Medicine-3rd-Edition-staibabussalamsula.ac_.id_.pdf"
     
-    #load PDF
-    load_pdf(url, pdfname)
+    #load PDF into documents
+    pdf_docs = load_pdf(url, pdfname)
+
+    #filter and preprocess documents
+    medical_filtered_docs= medical_filter_docs(pdf_docs)
